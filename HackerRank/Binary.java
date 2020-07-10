693c7d72458d081e1f637ea0e70d77455739a2fc
tatyanamtr <66910265+tatyanamtr@users.noreply.github.com>
2020-07-10 16:56:37 +0300

    Initial commit

---
+++ HackerRank/Binary.java
@@ -0,0 +1,31 @@
+package HackerRank;
+
+import java.util.Objects;
+
+public class Binary {
+        public static int binaryNumber(int n) {
+            String [] s = Integer.toBinaryString(n).split("");
+            int count = 0;
+            int max = 1;
+            for (int i =0; i < s.length; i++) {
+                if (Objects.equals(s[i], "1")) {
+                    count++;
+                    if (count > max) {
+                        max = count;
+                    }
+                } else {
+                    count = 0;
+                }
+            }
+            System.out.println(max);
+            return max;
+        }
+
+
+
+        public static void main(String[] args) {
+            binaryNumber(6);
+        }
+
+
+}
